7d9dd6049cff257cbc495f4e2ef97f804327401b
ElizeuFragas <elizeu.fragas@itec.ufpa.br>
2022-11-02 17:36:11 -0300

    Third

--- SNM.vhd
+++ SNM.vhd
@@ -13,72 +13,8 @@ end entity SNM;
 
 architecture behave of SNM is
 
-    type armazenamento is array(0 to 99) of std_logic_vector(nbits-1 downto 0);
-    type tipo_estado is (A, B, C);
-    signal a_estado : tipo_estado ;
-    constant memoria : armazenamento := ('32', '48', '58');
-    signal p : bit;
-    signal i_soma, dado : std_logic_vector(nbits-1 downto 0) := '0';
-    signal ende : interger;
+
     
 begin
-   estado: process(ende, dado, a_estado)
-   begin
-   case a_estado is
-    -- estado inicial
-    when A => 
-        p_estado <= B;
-    -- estado de espera
-    when B => 
-        if p = '0' or p = '1' then
-            p_estado <= C;
-        elsif;
-            p_estado <= B;
-        end if;
-    -- estado de verificação
-    when C =>
-        if ende = 99 then
-            ende <= '0';
-            p_estado <= E;
-        else
-            p_estado <= D;
-        end if;
-    -- estado de saída
-    when E =>
-        soma := i_soma;
-        p_estado <= B;
-    -- estado de soma           
-    when D => 
-        if p = '0' then
-            if memoria(ende) < 0 then
-                i_soma <= 
-   end process estado;
-
-  somador: process(clk)
-  begin
-    if falling_edge(clk) then
-        i_soma <= ende + 1;
-        ende <= i_soma;
-    elsif rising_edge(clk) then
-        if p = '0' then
-            if memoria(ende) < 0 then
-                i_soma <= i_soma + memoria(ende);
-                dado <= i_soma;            
-        elsif p = '1' then
-            if memoria(ende) >= 0 then
-                i_soma <= dado + memoria(ende);
-                dado <= i_soma;            
-        end if;
-    end if;
-  end process somador; 
-  proc_name: process(clk, rst)
-  begin
-    if rst = rst_val then
-        
-    elsif rising_edge(clk) then
-        
-    end if;
-  end process proc_name;
-    
-    
+   
 end architecture behave;
--- SNM_BO.vhd
+++ SNM_BO.vhd
@@ -1,18 +1,49 @@
-    entity SNM_BO is
-        port (
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
 
-        	type tipo_estado is (A, B, C, D, E);
-            state in : tipo_estado;
-            sum out : interger; 
-            
-            
-        );
-    end entity SNM_BO;
+entity SNM_BO is
+    
+    generic(
+        nbits : integer := 2
+    );
+    port (
+        in_data : in std_logic_vector(nbits-1 downto 0);
+        -- in_end in : integer;
+        p, id_op : in bit;
+        chk_endr : out bit;
+        sum : buffer std_logic_vector(nbits-1 downto 0)
+        
+    );
+end entity SNM_BO;
+architecture behave of SNM_BO is
+    signal a_sum : std_logic_vector(nbits-1 downto 0):= "00";
+    signal endr : integer := 0;
+begin
 
-    architecture behave of SNM_BO is
-        
-    begin
-        
-        
-        
-    end architecture behave;
+   sum_data: process(id_op)
+   begin
+     if id_op = '0' then
+         if p = '0' then
+             if in_data < "0" then
+                 sum <= std_logic_vector(signed(a_sum) + signed(in_data));
+                 a_sum <= sum;
+             end if;    
+         elsif p = '1' then
+             if in_data > "0" then
+                 sum <= std_logic_vector(signed(a_sum) + signed(in_data));
+                 a_sum <= sum;
+             end if;
+         end if;
+    else
+         if endr < 100 then
+              sum <= std_logic_vector(unsigned(std_logic_vector(to_unsigned(endr,2))) + unsigned(std_logic_vector(to_unsigned(1,2))));
+              endr <= to_integer(unsigned(sum));
+              chk_endr <= '0' ;
+         else
+              chk_endr <= '1';
+         end if;
+    end if;
+   end process sum_data;
+       
+end architecture behave;
